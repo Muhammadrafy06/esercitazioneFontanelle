7b9d1f4c37093011929e4696facb9ee40248a171
Muhammadrafy06 <muhammad.abdul@iisgalvanimi.edu.it>
2023-04-05 11:33:37 +0000

    secondo commit

--- Main.py
+++ Main.py
@@ -1,3 +1,13 @@
-#realizzare un sito web che permetta all'utente di avere una mappa di un quartiere con le fontalle. 
+#realizzare un sito web in flask che permetta all'utente di avere una mappa di un quartiere con le fontalle. 
 #L'utente seleziona da un menù a tendina il nome del quartiere, clicca sul bottone e ottiene la mappa del
 #quartiere prescelto con le fontanelle presenti
+from flask import Flask,render_template,request
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8080, debug=True)

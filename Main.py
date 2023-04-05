f25e4aa050f4e40b7427a6ec73ffb0937952d851
Muhammadrafy06 <muhammad.abdul@iisgalvanimi.edu.it>
2023-04-05 11:23:42 +0000

    primo commit

---
+++ Main.py
@@ -0,0 +1,3 @@
+#realizzare un sito web che permetta all'utente di avere una mappa di un quartiere con le fontalle. 
+#L'utente seleziona da un menù a tendina il nome del quartiere, clicca sul bottone e ottiene la mappa del
+#quartiere prescelto con le fontanelle presenti
